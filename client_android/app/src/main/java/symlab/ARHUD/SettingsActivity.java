18a00d7011f1442f5eb38e5a663e675a76338f7a
Jacky Cao <jackycao@outlook.com>
2020-07-22 08:18:00 +0300

    implemented basic fragment for server settings
    - preparing to change package name to reflect the changes being made

--- client_android/app/src/main/java/symlab/ARHUD/SettingsActivity.java
+++ client_android/app/src/main/java/symlab/ARHUD/SettingsActivity.java
@@ -1,16 +1,40 @@
 package symlab.ARHUD;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import symlab.ARHUD.fragments.settingServer;
+
 
 public class SettingsActivity extends Activity {
+    TextView serverFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        serverFragment = (TextView)findViewById(R.id.settingServer);
 
+        serverFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new settingServer());
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit(); // save the changes
     }
 }
---
+++ client_android/app/src/main/java/symlab/ARHUD/fragments/settingServer.java
@@ -0,0 +1,21 @@
+package symlab.ARHUD.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import symlab.ARHUD.R;
+
+public class settingServer extends Fragment {
+    View view;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.settings_server_fragment, container, false);
+        Toast.makeText(getActivity(), "First Fragment", Toast.LENGTH_LONG).show();
+        return view;
+    }
+
+}

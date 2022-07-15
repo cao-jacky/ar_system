8ebe430a73882dff1b264e36c4eaa372134a2d2f
Jacky Cao <jackycao@outlook.com>
2022-07-15 11:10:04 +0300

    updating python client

--- client_simple/client.py
+++ client_simple/client.py
@@ -10,11 +10,16 @@ import socket
 import mimetypes
 import cv2
 
-payload_size = 30000
-
+from threading import Thread
 
 def print_json(client_id, frame_no, message):
     print(f'{{\\"service_name\\": \\"client\\", \\"id\\": \\"{client_id}\\", \\"frame_no\\": \\"{frame_no}\\", \\"timestamp\\": \\"{time.time_ns()/1000/1000}\\", \\"message\\": \\"{message}\\"}}')
+
+
+def listen_data(sock):
+    while True:
+        data, addr = sock.recvfrom(60000)  # buffer size is 1024 bytes
+        print("received message: %s" % data)
 
 
 def send_data(client_id, frame_type, frame_no, frame_buffer, sock, main_ip, main_port):
@@ -25,20 +30,23 @@ def send_data(client_id, frame_type, frame_no, frame_buffer, sock, main_ip, main
     else:
         frame_bytes = frame_buffer.tostring()
     frame_len = len(frame_bytes)
-    
+
     payload_size = frame_len + 16
     frame_array = bytearray(payload_size)
 
-    frame_array[0:4] = bytearray(client_id, 'utf-8')    # client_id
+    frame_array[0:4] = bytearray(client_id, 'utf-8')       # client_id
     frame_array[4:8] = (frame_no).to_bytes(4, 'little')    # frame_id
-    frame_array[8:12] = (frame_type).to_bytes(4, 'little') # frame_type
+    frame_array[8:12] = (frame_type).to_bytes(4, 'little')  # frame_type
     frame_array[12:16] = frame_len.to_bytes(4, 'little')   # frame_len
-    frame_array[16:frame_len+16] = frame_bytes          # frame_data
+    frame_array[16:frame_len+16] = frame_bytes             # frame_data
 
+    if (frame_type == 2) & (frame_len < 2000):
+        return
     sock.sendto(frame_array, (main_ip, int(main_port)))
-    
+
     print_json(
         client_id, frame_no, f'Sent Frame {frame_no} of total payload length {payload_size} to main service for processing')
+
 
 def main(argv):
     # defining server details
@@ -72,19 +80,28 @@ def main(argv):
         client_id = ''.join(random.choices(
             string.ascii_letters + string.digits, k=4))
         # print(f'{time.time_ns()/1000} [STATUS {client_id} client] Client created and assigned ID of {client_id}')
-        print_json(client_id, 0, f'Client created and assigned ID of {client_id}')
+        print_json(
+            client_id, 0, f'Client created and assigned ID of {client_id}')
         print_json(
             client_id, 0, f'Details of main service provided: {server_ip}:{server_port}')
 
         # Preparing UDP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(("", 0))
+        client_assigned_port = sock.getsockname()[1]
         print_json(
-            client_id, 0, f'Port {sock.getsockname()[1]} is binded to the client')
+            client_id, 0, f'Port {client_assigned_port} is bound to the client')
 
         send_data(client_id, 0, 0, "", sock, server_ip, server_port)
         print_json(
             client_id, 0, f'Sending initial echo message to main to self-register client details')
+
+        # create separate thread to listen for messages
+        # create threads
+        thread = Thread(target=listen_data, args=(sock,))
+
+        # start the threads
+        thread.start()
 
         # Loading file with the name provided by the user
         mimetypes.init()
@@ -125,14 +142,18 @@ def main(argv):
                         encoding_params = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
                         retval, frame_buffer = cv2.imencode(
                             '.jpg', frame_resized, encoding_params)
-                        # cv2.imwrite(f'frame.jpg', frame_buffer)
+                        if frame_count < 10:
+                            cv2.imwrite(
+                                f'frame{frame_count}.jpg', frame_resized)
+                        else:
+                            break
                         time_end = time.time_ns()/1000/1000
                         print_json(
                             client_id, frame_count, f'Preprocessing of Frame {frame_count} took {time_end-time_start} ms')
 
                         send_data(client_id, 2, frame_count, frame_buffer,
                                   sock, server_ip, server_port)
-                        
+
                         print(server_ip, server_port)
 
                         frame_count += 1
